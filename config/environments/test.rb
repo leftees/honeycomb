fdab79645db437bb8e65b6539baf1633793c731e
Robert Fox <robertjonfox@gmail.com>
2014-10-17 12:17:53 -0400

    Add CAS configs to test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,13 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  
+  # Custom configuration
+  config.ldap_lookup_flag                 = true
+  config.honeycomb_upload_path             = '/shared/data/honeycomb_files'
+  config.honeycomb_cas_base                = 'https://cas.library.nd.edu/cas'
+  config.honeycomb_cas_validate            = 'https://cas.library.nd.edu/cas/serviceValidate'
+  config.honeycomb_cas_logout              = 'https://cas.library.nd.edu/cas/logout'
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
