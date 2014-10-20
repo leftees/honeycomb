3032b4b810122073cadf1dde9160faf9a055af21
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-20 15:30:46 -0400

    added some more specs to the save item spec.

--- spec/services/save_item_spec.rb
+++ spec/services/save_item_spec.rb
@@ -20,15 +20,28 @@ RSpec.describe SaveItem, type: :model do
     subject
   end
 
-
   it "sets the title to be the uploaded filename when the item is a new record?" do
-    item.stub(:image_file_name).and_return("filename")
-    item.stub(:title).and_return("")
-
     expect(item).to receive(:new_record?).and_return(true)
-    expect(item).to receive("title=").with(item.image_file_name)
+    expect(item).to receive(:title).and_return("")
+    expect(item).to receive(:image_file_name).and_return("filename")
+    expect(item).to receive("title=").with('filename')
 
     subject
   end
 
+  it "uses the existing title if it has a title " do
+    expect(item).to receive(:new_record?).and_return(true)
+    expect(item).to receive(:title).and_return("title")
+
+    expect(item).to_not receive("title=")
+
+    subject
+  end
+
+
+  it "does not set the title when it is not a new record " do
+    expect(item).to_not receive("title=")
+
+    subject
+  end
 end
