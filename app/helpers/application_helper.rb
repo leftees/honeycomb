50702795601806850951151bc470909857b8365c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2014-10-23 11:21:13 -0400

    added flash message handler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
 
     res
   end
+
+  def display_notices
+    content = raw("")
+    if flash[:notice].present?
+      content += content_tag(:div, raw(flash[:notice]), class: "alert alert-info")
+    end
+    if flash[:alert].present?
+      content += content_tag(:div, raw(flash[:alert]), class: "alert")
+    end
+    if flash[:success].present?
+      content += content_tag(:div, raw(flash[:success]), class: "alert alert-success")
+    end
+    if flash[:error].present?
+      content += content_tag(:div, raw(flash[:error]), class: "alert alert-error")
+    end
+    content_tag(:div, content, id: "notices")
+  end
+
 end
