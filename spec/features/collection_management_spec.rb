585468feec88fa0cc99c258a33fb602596e24d1f
Robert Fox <robertjonfox@gmail.com>
2014-10-28 10:37:03 -0400

    Add feature specs for the creation of collections

--- spec/features/collection_management_spec.rb
+++ spec/features/collection_management_spec.rb
@@ -1,9 +1,28 @@
 require "rails_helper"
+require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 feature "Collection creation" do
-  scenario "Admin creates a new collection" do
+
+  before(:each) do
+    @user = User.new(username: 'jhartzle')
+    @user.save!
+    login_as @user
+  end
+  
+  scenario "Admin starts can access new collection page" do
     visit "/collections"
-    click_button "New"
+    click_link "New"
     expect(page).to have_text("Title")
+  end
+
+  scenario "Admin can create new collection" do
+    visit "/collections/new"
+    within("#new_collection") do
+      fill_in 'Title', with: 'Test Collection'
+    end
+    click_button 'Create Collection'
+    expect(page).to have_content 'Title: Test Collection'
   end
 end
