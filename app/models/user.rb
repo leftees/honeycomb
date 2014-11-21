82a144cbf50c17a482f08ab45cb5084ef4464190
Robert Fox <rfox2@nd.edu>
2014-11-21 14:40:53 -0500

    Add some spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
 
   has_many :collection_users
   has_many :collections, :through => :collection_users
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  
   devise :cas_authenticatable, :trackable
 
   before_validation :map_user
-  # Setup accessible (or protected) attributes for your model
 
   validates :username, :email, :uniqueness => true
   validates_presence_of :username
@@ -24,7 +21,9 @@ class User < ActiveRecord::Base
   end
 
   protected
+
   def map_user
     MapUserToApi.call(self)
   end
+
 end
