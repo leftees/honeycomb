08e9a0fe36116bd43e75e78672170af508a735ac
Dan Wolfe <dwolfe2@nd.edu>
2014-11-21 13:27:05 -0500

    Fixed user_is_curator(collection) method and wrote tests for SitePermission.

--- app/policy/user_is_curator.rb
+++ app/policy/user_is_curator.rb
@@ -1,7 +1,7 @@
 class UserIsCurator
 
   def self.call(user, collection)
-    new(user,collection).is_curator?
+    new(user, collection).is_curator?
   end
 
   def initialize(user, collection)
@@ -10,7 +10,7 @@ class UserIsCurator
   end
 
   def is_curator?
-    if CollectionUser.where(collection_id: collection.id, user_id: user.id)
+    if CollectionUser.where(collection_id: collection.id, user_id: user.id).first
       true
     else
       false
