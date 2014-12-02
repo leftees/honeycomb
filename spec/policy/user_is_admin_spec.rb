124986a61d5f5cb722ee52e48333216e835c839b
Dan Wolfe <dwolfe2@nd.edu>
2014-12-02 11:21:14 -0500

    Adding some missing tests for policies.

---
+++ spec/policy/user_is_admin_spec.rb
@@ -0,0 +1,14 @@
+describe UserIsAdmin do
+  subject {described_class.call(user)}
+  let(:user) { double(User, id: "1") }
+
+  it "returns when a User is found" do
+    expect(user).to receive(:admin?).and_return(true)
+    expect(subject).to be(true)
+  end
+
+  it "returns false when a User is not found" do
+    expect(user).to receive(:admin?).and_return(false)
+    expect(subject).to be(false)
+  end
+end
---
+++ spec/policy/user_is_curator_spec.rb
@@ -0,0 +1,15 @@
+describe UserIsCurator do
+  subject {described_class.call(user, collection)}
+  let(:user) { double(User, id: "1") }
+  let(:collection) { double(Collection, id: "1") }
+
+  it "returns when a CollectionUser is found" do
+    CollectionUser.stub_chain(:where, :first).and_return(true)
+    expect(subject).to be(true)
+  end
+
+  it "returns false when a CollectionUser is not found" do
+    CollectionUser.stub_chain(:where, :first).and_return(false)
+     expect(subject).to be(false)
+  end
+end
