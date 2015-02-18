c94216fc36461b0d34cd24087b144e48d9d0369e
Jon Hartzler <jon.hartzler@gmail.com>
2015-02-18 10:09:24 -0500

    added the error messages object from waggle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,39 +26,8 @@ module ApplicationHelper
     zoom_content = content_tag(:div, content, options)
   end
 
-  def top_nav_links
-    res = []
-    if defined?(collection)
-      res << link_to(collection.title, collection_items_path(collection))
-    end
-    if permission.user_is_administrator? || permission.user_is_admin_in_masquerade?
-      res << link_to("Users", users_path)
-    end
-    if masquerade.masquerading?
-      res << link_to("Cancel Masquerade", cancel_masquerades_path)
-    end
-    res
-  end
-
-  def display_notices
-    content = raw("")
-    if flash[:notice].present?
-      content += content_tag(:div, raw(flash[:notice]), class: "alert alert-info")
-    end
-    if flash[:alert].present?
-      content += content_tag(:div, raw(flash[:alert]), class: "alert alert-warning")
-    end
-    if flash[:success].present?
-      content += content_tag(:div, raw(flash[:success]), class: "alert alert-success")
-    end
-    if flash[:error].present?
-      content += content_tag(:div, raw(flash[:error]), class: "alert alert-danger")
-    end
-    content_tag(:div, content, id: "notices")
-  end
-
   def display_errors(obj)
-    Waggle::ErrorMessages.new(obj).display_error
+    ErrorMessages.new(obj).display_error
   end
 
   def masquerade
@@ -67,10 +36,6 @@ module ApplicationHelper
 
   def permission
     @permission ||= SitePermission.new(current_user, self)
-  end
-
-  def collection_nav(collection, active)
-    Waggle::SideNav.new(collection).display(active)
   end
 
   def collection_title(collection,  section_title = nil)
