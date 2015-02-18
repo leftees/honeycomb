77ddf6c1bc040a5114adf436d91a50fecf646c32
Jaron Kennel <jaronkk@gmail.com>
2015-02-18 12:34:42 -0500

    Store user location to properly redirect after sign in
    Conflicts:
    	app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ require 'error_helper'
 class ApplicationController < ActionController::Base
   include ErrorHelper
 
-  #before_action :redirect_to_sign_in, unless: :user_signed_in?, :except => [:catch_500, :catch_404]
+  before_action :store_location
+  before_action :redirect_to_sign_in, unless: :user_signed_in?, :except => [:catch_500, :catch_404]
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -17,6 +18,27 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+    def store_location
+      # store last url - this is needed for post-login redirect to whatever the user last visited.
+      return unless request.get?
+      if (!is_user_path? && !request.xhr?) # don't store ajax calls
+        session[:previous_url] = request.fullpath
+      end
+    end
+
+    def is_user_path?
+      match = request.path =~ /^\/users\/.*/
+      match.present?
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      if session["#{resource_or_scope}_return_to"]
+        session["#{resource_or_scope}_return_to"]
+      else
+        session[:previous_url] || root_path
+      end
+    end
 
     def permission
       @permission ||= SitePermission.new(current_user, self)
