357f6b5d219099b9c355226db39dc39cfd120472
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-02-20 13:43:07 -0500

    add service to ensure exhibit creation.

---
+++ app/services/ensure_collection_has_exhibit.rb
@@ -0,0 +1,25 @@
+class EnsureCollectionHasExhibit
+  attr_reader :collection
+
+  def self.call(collection)
+    new(collection).ensure
+  end
+
+  def initialize(collection)
+    @collection = collection
+  end
+
+  def ensure
+    if !collection.exhibit
+      create!
+    end
+
+    collection.exhibit
+  end
+
+  private
+
+    def create!
+      collection.create_exhibit(title: collection.title)
+    end
+end
