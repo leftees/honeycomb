652de15075f17ace2ef5785372b62f98a0b472f9
Jaron Kennel <jaronkk@gmail.com>
2015-02-25 15:47:13 -0500

    Moved section spacer styles

--- app/assets/javascripts/components/new_section_dropzone.js.jsx
+++ app/assets/javascripts/components/new_section_dropzone.js.jsx
@@ -13,7 +13,27 @@ var NewSectionDropzone = React.createClass({
     };
   },
   innerStyle: function() {
+    var styles = {
+      color: 'rgba(0, 0, 0, 0)',
+      border: '#333 dashed',
+      height: '100%',
+      width: '40px',
+      transitionProperty: 'all',
+      transitionDuration: '.5s',
+      transitionTimingFunction: 'cubic-bezier(0, 1, 0.5, 1)',
+      overflow: 'hidden',
+    };
+    if (this.state.hover) {
+      styles.width = '200px';
+      styles.backgroundColor = '#ededed';
+      styles.color = '#333';
+      styles.padding = '14px';
+    }
+    return styles;
+  },
+  contentStyle: function() {
     return {
+      opacity: (this.state.hover ? 1 : 0)
     };
   },
   classes: function() {
@@ -32,6 +52,9 @@ var NewSectionDropzone = React.createClass({
   active: function() {
     return this.props.currentDragItem;
   },
+  activeHover: function() {
+    return this.active() && this.state.hover;
+  },
   onDrop: function() {
     if (this.active()) {
       return this.props.onDrop(this.props.currentDragItem, this.props.new_index);
@@ -42,7 +65,7 @@ var NewSectionDropzone = React.createClass({
     return (
       <div style={this.style()} className={this.classes()} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onMouseUp={this.onDrop}>
         <div className="section-spacer-inner" style={this.innerStyle()}>
-          <div className="section-spacer-content center">
+          <div className="section-spacer-content center" style={this.contentStyle()}>
             <h4>Add New Section</h4>
             <p><i className="glyphicon glyphicon-picture super-large-icon"></i></p>
             <p>Drag a saved item or<br/><a href={url}><strong>Add Text</strong></a></p>
