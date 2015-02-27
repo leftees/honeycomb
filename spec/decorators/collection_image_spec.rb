451b3b3f5e54f8ef6b44e4d3913add9cea1615f1
Jaron Kennel <jaronkk@gmail.com>
2015-02-27 15:59:24 -0500

    Fixed a spec

--- spec/decorators/collection_image_spec.rb
+++ spec/decorators/collection_image_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe CollectionImage do
   let(:item_with_image) { double(Item, honeypot_image: honeypot_image) }
   let(:item_with_no_image) { double(Item, honeypot_image: nil) }
 
-  let(:honeypot_image) { double(HoneypotImage, url: "http://image.image.com/image")}
+  let(:honeypot_image) { double(HoneypotImage, url: "http://image.image.com/image", image_json: {contentUrl: 'http://example.com/image.jpg'})}
 
   it "uses the item decorator to call the image display" do
     allow(collection).to receive(:items).and_return([item_with_image])
@@ -20,13 +20,13 @@ RSpec.describe CollectionImage do
   it "dislpays the first image found" do
     allow(collection).to receive(:items).and_return([item_with_image])
 
-    expect(subject.display).to eq("<div data-react-class=\"Thumbnail\" data-react-props=\"{&quot;image&quot;:&quot;http://image.image.com/image&quot;}\"></div>")
+    expect(subject.display).to eq("<div data-react-class=\"Thumbnail\" data-react-props=\"{&quot;image&quot;:{&quot;contentUrl&quot;:&quot;http://example.com/image.jpg&quot;}}\"></div>")
   end
 
   it "displays the second image if the first item does not have an image" do
     allow(collection).to receive(:items).and_return([item_with_no_image, item_with_image])
 
-    expect(subject.display).to eq("<div data-react-class=\"Thumbnail\" data-react-props=\"{&quot;image&quot;:&quot;http://image.image.com/image&quot;}\"></div>")
+    expect(subject.display).to eq("<div data-react-class=\"Thumbnail\" data-react-props=\"{&quot;image&quot;:{&quot;contentUrl&quot;:&quot;http://example.com/image.jpg&quot;}}\"></div>")
   end
 
   it "displays nothing if there are no images" do
