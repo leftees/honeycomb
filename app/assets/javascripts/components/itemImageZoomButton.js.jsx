df50a3f8683349807c664d755700d17c29e46bb0
Jaron Kennel <jaronkk@gmail.com>
2015-03-02 14:10:16 -0500

    Nicer looking item image box

--- app/assets/javascripts/components/itemImageZoomButton.js.jsx
+++ app/assets/javascripts/components/itemImageZoomButton.js.jsx
@@ -25,7 +25,7 @@ var ItemImageZoomButton = React.createClass({
     var fullPage = !!this.state.zoomClicked;
     return (
       <div className="hc-image-zoom-button-outer">
-        <button className="btn btn-default hc-image-zoom-button" data-target={zoomID} onClick={this.handleClick}><span className="glyphicon glyphicon-zoom-in"></span></button>
+        <button style={{backgroundColor: 'white'}} className="btn btn-default btn-raised hc-image-zoom-button" data-target={zoomID} onClick={this.handleClick}><span className="glyphicon glyphicon-zoom-in"></span></button>
         <div className="hc-image-zoom-button-inner">
           <OpenseadragonViewer image={this.props.image} containerID={zoomID} fullPage={fullPage} />
         </div>
