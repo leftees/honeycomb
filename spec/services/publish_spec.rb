626d7e0b4c9c241930eb01a1624a11cf35839929
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-04 11:01:32 -0500

    update the spec

--- spec/services/publish_spec.rb
+++ spec/services/publish_spec.rb
@@ -1,29 +1,43 @@
 require 'rails_helper'
 
 describe Publish do
-  subject { described_class.call(object) }
+  subject { described_class.new(object) }
   let(:object) { double(Object, "published=" => true, save: true) }
 
-  it "sets the object to be published" do
-    expect(object).to receive(:published=).with(true)
-    subject
+  describe "#publish!" do
+
+    it "sets the object to be published" do
+      expect(object).to receive(:published=).with(true)
+      subject.publish!
+    end
+
+    it "saves the object" do
+      expect(object).to receive(:save).and_return(true)
+      subject.publish!
+    end
+
+    it "returns true of the object is saved" do
+      expect(subject.publish!).to be true
+    end
+
+    it "returns false if the object save fails " do
+      allow(object).to receive("save").and_return(false)
+      expect(subject.publish!).to be false
+    end
   end
 
-  it "saves the object" do
-    expect(object).to receive(:save).and_return(true)
-    subject
+  describe "initilaize" do
+    it "raises an error if the object is not duck typed" do
+      expect { described_class.new(Object.new) }.to raise_error
+    end
   end
 
-  it "returns true of the object is saved" do
-    expect(subject).to be true
-  end
 
-  it "returns false if the object save fails " do
-    allow(object).to receive("save").and_return(false)
-    expect(subject).to be false
-  end
+  describe "#call" do
 
-  it "raises an error if the object is not duck typed" do
-    expect { described_class.call(Object.new) }.to raise_error
+    it "calls publish!" do
+      expect_any_instance_of(Publish).to receive(:publish!)
+      described_class.call(object)
+    end
   end
 end
