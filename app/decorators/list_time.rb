d117b0d1148ebe3528bef7b130c0b90bbd7e1ab2
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-06 14:31:31 -0500

    update the last modified time display to be better

---
+++ app/decorators/list_time.rb
@@ -0,0 +1,16 @@
+require 'draper'
+
+class ListTime < Draper::Decorator
+
+  def self.display(time)
+    new(time).display
+  end
+
+  def display
+    if object.today?
+      h.l(object.localtime, format: :list_time)
+    else
+      h.l(object.localtime, format: :list_date)
+    end
+  end
+end
