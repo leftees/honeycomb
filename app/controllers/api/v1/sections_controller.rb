2b304b4a25cb5b22e9ebfb8e33a26d80f563be6b
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-11 10:51:21 -0400

    add routes for the show page

---
+++ app/controllers/api/v1/sections_controller.rb
@@ -0,0 +1,10 @@
+module API
+  module V1
+    class SectionsController < APIController
+
+      def show
+        @section = SectionQuery.new.public_find(params[:id])
+      end
+    end
+  end
+end
