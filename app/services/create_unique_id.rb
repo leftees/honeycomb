618608c60a0124e8f07172ad63d26e4bf0a55aaf
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-17 10:38:28 -0400

    change create unique id to do the work of saving and testing

--- app/services/create_unique_id.rb
+++ app/services/create_unique_id.rb
@@ -4,15 +4,32 @@ class CreateUniqueId
   attr_reader :object
 
   def self.call(object)
-    new(object).create
+    new(object).create!
   end
 
   def initialize(object)
     @object = object
+    validate_interface!
   end
 
-  def create
-    Digest::MD5.hexdigest("#{object.id}-#{object.class}")[0...10]
+  def create!
+    if object.unique_id.nil?
+      object.unique_id = unique_id
+      return object.save
+    end
+
+    true
   end
 
+  private
+
+    def unique_id
+      Digest::MD5.hexdigest("#{object.id}-#{object.class}")[0...10]
+    end
+
+    def validate_interface!
+      if !(object.respond_to?('unique_id=') && object.respond_to?(:unique_id) && object.respond_to?(:save))
+        raise "Object passed to CreateUniqueId is not valid"
+      end
+    end
 end
