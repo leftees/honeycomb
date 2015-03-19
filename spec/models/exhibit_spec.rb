771158220ae70ad516e9ca08263c27e6e77dc1a3
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-19 10:41:13 -0400

    update exhibit to save images.

--- spec/models/exhibit_spec.rb
+++ spec/models/exhibit_spec.rb
@@ -9,12 +9,6 @@ RSpec.describe Exhibit do
     end
   end
 
-  [:title].each do | field |
-    it "requires the field, #{field}" do
-      expect(subject).to have(1).error_on(field)
-    end
-  end
-
   describe '#items_json_url' do
     it 'is a url to the honeycomb server' do
       subject.collection_id = 100
@@ -26,6 +20,21 @@ RSpec.describe Exhibit do
     it 'is a url to the honeycomb server' do
       subject.collection_id = 100
       expect(subject.item_json_url(5)).to eq("/api/collections/100/items/5.json?include=image")
+    end
+  end
+
+  describe "#has honeypot image interface" do
+
+    it "responds to image" do
+      expect(subject).to respond_to(:image)
+    end
+
+    it "responds to honeypot_image" do
+      expect(subject).to respond_to(:honeypot_image)
+    end
+
+    it "responds to collection" do
+      expect(subject).to respond_to(:collection)
     end
   end
 
