6d615b67bb32ca5aca2049e1d9ab86ba8587d9f9
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-20 09:16:10 -0400

    temp to get migrations to work.

--- db/migrate/20150309144044_add_unique_id_to_collections.rb
+++ db/migrate/20150309144044_add_unique_id_to_collections.rb
@@ -1,7 +1,7 @@
 class AddUniqueIdToCollections < ActiveRecord::Migration
   def change
-    add_column :collections, :unique_id, :string
-    add_index :collections, :unique_id
+#    add_column :collections, :unique_id, :string
+#    add_index :collections, :unique_id
 
     Collection.all.each do | s |
       SaveCollection.call(s, {})
