1392fa6b0a9a332aa824d61c62c6d47fdc01369c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-03-24 09:48:59 -0400

    make the spec more readable.

--- spec/decorators/v1/section_json_decorator_spec.rb
+++ spec/decorators/v1/section_json_decorator_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe V1::SectionJSONDecorator do
   subject {described_class.new(section)}
 
-  let(:collection) { double(Collection, id: 1, unique_id: "colasdf", title: 'collection title') }
-  let(:section) { double(Section, id: 1, description: nil, unique_id: "adsf", caption: 'caption', title: 'section title', collection: collection, showcase: showcase )}
-  let(:showcase) { double(Showcase, id: 1, unique_id: "showadsf", title: 'title title')}
-  let(:json) { double }
+  #let(:collection) { double(Collection, id: 1, unique_id: "colasdf", title: 'collection title') }
+  let(:section) { double(Section)}
+
+  #, id: 1, description: nil, unique_id: "adsf", caption: 'caption', title: 'section title', collection: collection, showcase: showcase )}
+  #let(:showcase) { double(Showcase, id: 1, unique_id: "showadsf", title: 'title title')}
+
 
   describe "generic fields" do
     [:id, :caption, :unique_id, :item, :updated_at, :order].each do | field |
@@ -38,6 +40,7 @@ RSpec.describe V1::SectionJSONDecorator do
   end
 
   describe "#at_id" do
+    let(:section) { double(Section, unique_id: 'adsf' )}
 
     it "returns the path to the id" do
       expect(subject.at_id).to eq("http://test.host/v1/sections/adsf")
@@ -45,13 +48,17 @@ RSpec.describe V1::SectionJSONDecorator do
   end
 
   describe "#collection_url" do
+    let(:section) { double(Section, collection: collection )}
+    let(:collection) { double(Collection, unique_id: 'coladsf' )}
 
     it "returns the path to the items" do
-      expect(subject.collection_url).to eq("http://test.host/v1/collections/colasdf")
+      expect(subject.collection_url).to eq("http://test.host/v1/collections/coladsf")
     end
   end
 
   describe "#showcase_url" do
+    let(:section) { double(Section, showcase: showcase )}
+    let(:showcase) { double(Showcase, unique_id: 'showadsf' )}
 
     it "returns the path to the items" do
       expect(subject.showcase_url).to eq("http://test.host/v1/showcases/showadsf")
@@ -59,12 +66,20 @@ RSpec.describe V1::SectionJSONDecorator do
   end
 
   describe "#description" do
+    let(:section) { double(Section, description: nil )}
+
     it "converts null to empty string" do
       expect(subject.description).to eq("")
+    end
+
+    it "delegates to section" do
+      expect(section).to receive(:description).and_return('desc')
+      expect(subject.description).to eq('desc')
     end
   end
 
   describe "#slug" do
+    let(:section) { double(Section, title: 'title')}
 
     it "Calls the slug generator" do
       expect(CreateURLSlug).to receive(:call).with(section.title).and_return('slug')
@@ -72,8 +87,24 @@ RSpec.describe V1::SectionJSONDecorator do
     end
   end
 
+  describe '#next' do
+
+    it "calls the query object" do
+      expect_any_instance_of(SectionQuery).to receive(:next).with(section)
+      subject.next
+    end
+  end
+
+  describe '#previous' do
+
+    it "calls the query object" do
+      expect_any_instance_of(SectionQuery).to receive(:previous).with(section)
+      subject.previous
+    end
+  end
 
   describe "#display" do
+    let(:json) { double }
 
     it "calls the partial for the display" do
       expect(json).to receive(:partial!).with("/v1/sections/section", {:section_object => section })
