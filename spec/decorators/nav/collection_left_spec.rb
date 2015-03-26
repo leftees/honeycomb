d4982a73ae2738a585daea7afa20a3cc7e4655da
Robert Fox <rfox2@nd.edu>
2015-03-26 13:55:58 -0400

    Accomodate for creating beehive urls

--- spec/decorators/nav/collection_left_spec.rb
+++ spec/decorators/nav/collection_left_spec.rb
@@ -1,27 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Nav::CollectionLeft do
-  subject { described_class.new(collection)}
+  let(:exhibit) { double(Exhibit, id: 1) }
   let(:collection) { double(Collection, id:  1, title: 'Title', exhibit: exhibit) }
-  let(:exhibit) { double(Exhibit, id: 1)}
+  subject { Nav::CollectionLeft.new(collection) }
 
-  it "renders the correct template" do
-    expect(subject.h).to receive(:render).with({:partial=>"shared/collection_left_nav", locals: { nav: subject }})
+  before(:each) do
+    allow(collection).to receive(:collection).and_return(collection)
+    allow(collection).to receive(:unique_id)
+  end
+
+  it 'renders the correct template' do
+    expect(subject.h).to receive(:render)
+      .with(partial: 'shared/collection_left_nav', locals: { nav: subject })
     subject.display(:section)
   end
 
-  it "returns the object as collection" do
+  it 'returns the object as collection' do
     expect(subject.collection).to eq(collection)
   end
 
-  it "sets selected when the left nav section is active" do
+  it 'sets selected when the left nav section is active' do
     subject.display(:section)
-    expect(subject.active_section_css(:section)).to eq("selected")
+    expect(subject.active_section_css(:section)).to eq('selected')
   end
 
-  it "sets empty when the left nav section is not active" do
+  it 'sets empty when the left nav section is not active' do
     subject.display(:section)
-    expect(subject.active_section_css(:other_section)).to eq("")
+    expect(subject.active_section_css(:other_section)).to eq('')
   end
-
 end
