9d9e1ca6ad4934db0d51fd75bbe3ca05aab3a89b
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-04-07 16:12:13 -0400

    update the drop zone to be in a modal

---
+++ app/assets/javascripts/components/loadingImage.js.jsx
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+
+var LoadingImage = React.createClass({
+
+  render: function () {
+    return (<div className="loading"><img src="/images/ajax-loader.gif" /></div>)
+  }
+});
