bc30d9c3b89e8d434add6e36de4400d86ab7dd0f
Robert Fox <rfox2@nd.edu>
2015-04-16 10:18:47 -0400

    Remove extra file

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = '2623bd51a7b11b8e98fe8186e67898b3'
-  config.host    = 'errbit.library.nd.edu'
-  config.port    = 443
-  config.secure  = config.port == 443
-end
