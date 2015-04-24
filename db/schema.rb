57d9185cc1a4cd2487d182d384915d2efa3cef8a
Jaron Kennel <jaronkk@gmail.com>
2015-04-24 10:27:33 -0400

    Schema dump changed in Rails 4.2, updating schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150416184347) do
 
-  create_table "collection_users", force: true do |t|
-    t.integer  "user_id",       null: false
-    t.integer  "collection_id", null: false
+  create_table "collection_users", force: :cascade do |t|
+    t.integer  "user_id",       limit: 4, null: false
+    t.integer  "collection_id", limit: 4, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -23,57 +23,57 @@ ActiveRecord::Schema.define(version: 20150416184347) do
   add_index "collection_users", ["collection_id"], name: "index_collection_users_on_collection_id", using: :btree
   add_index "collection_users", ["user_id"], name: "index_collection_users_on_user_id", using: :btree
 
-  create_table "collections", force: true do |t|
-    t.string   "title"
+  create_table "collections", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "deleted",     default: false
-    t.text     "description"
-    t.string   "unique_id"
-    t.boolean  "published"
+    t.boolean  "deleted",     limit: 1,     default: false
+    t.text     "description", limit: 65535
+    t.string   "unique_id",   limit: 255
+    t.boolean  "published",   limit: 1
   end
 
   add_index "collections", ["published"], name: "index_collections_on_published", using: :btree
   add_index "collections", ["unique_id"], name: "index_collections_on_unique_id", using: :btree
 
-  create_table "exhibits", force: true do |t|
-    t.text     "title"
-    t.text     "description"
-    t.integer  "collection_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
+  create_table "exhibits", force: :cascade do |t|
+    t.text     "title",              limit: 65535
+    t.text     "description",        limit: 65535
+    t.integer  "collection_id",      limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
   end
 
-  create_table "honeypot_images", force: true do |t|
-    t.integer  "item_id"
-    t.string   "title"
-    t.text     "json_response"
+  create_table "honeypot_images", force: :cascade do |t|
+    t.integer  "item_id",       limit: 4
+    t.string   "title",         limit: 255
+    t.text     "json_response", limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "showcase_id"
-    t.integer  "exhibit_id"
+    t.integer  "showcase_id",   limit: 4
+    t.integer  "exhibit_id",    limit: 4
   end
 
   add_index "honeypot_images", ["item_id"], name: "index_honeypot_images_on_item_id", using: :btree
 
-  create_table "items", force: true do |t|
-    t.text     "title"
-    t.text     "description"
+  create_table "items", force: :cascade do |t|
+    t.text     "title",              limit: 65535
+    t.text     "description",        limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "collection_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
+    t.integer  "collection_id",      limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
-    t.text     "sortable_title"
-    t.integer  "parent_id"
-    t.string   "manuscript_url"
-    t.boolean  "published"
-    t.string   "unique_id"
-    t.text     "transcription"
+    t.text     "sortable_title",     limit: 65535
+    t.integer  "parent_id",          limit: 4
+    t.string   "manuscript_url",     limit: 255
+    t.boolean  "published",          limit: 1
+    t.string   "unique_id",          limit: 255
+    t.text     "transcription",      limit: 65535
   end
 
   add_index "items", ["collection_id"], name: "index_items_on_collection_id", using: :btree
@@ -81,65 +81,65 @@ ActiveRecord::Schema.define(version: 20150416184347) do
   add_index "items", ["published"], name: "index_items_on_published", using: :btree
   add_index "items", ["unique_id"], name: "index_items_on_unique_id", using: :btree
 
-  create_table "sections", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image"
-    t.integer  "item_id"
-    t.integer  "order"
-    t.text     "caption"
-    t.integer  "showcase_id"
-    t.string   "unique_id"
+  create_table "sections", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.string   "image",       limit: 255
+    t.integer  "item_id",     limit: 4
+    t.integer  "order",       limit: 4
+    t.text     "caption",     limit: 65535
+    t.integer  "showcase_id", limit: 4
+    t.string   "unique_id",   limit: 255
     t.datetime "updated_at"
     t.datetime "created_at"
   end
 
   add_index "sections", ["unique_id"], name: "index_sections_on_unique_id", using: :btree
 
-  create_table "showcases", force: true do |t|
-    t.text     "title"
-    t.text     "description"
-    t.integer  "exhibit_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
+  create_table "showcases", force: :cascade do |t|
+    t.text     "title",              limit: 65535
+    t.text     "description",        limit: 65535
+    t.integer  "exhibit_id",         limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
     t.datetime "updated_at"
     t.datetime "created_at"
-    t.boolean  "published"
-    t.string   "unique_id"
-    t.integer  "order"
+    t.boolean  "published",          limit: 1
+    t.string   "unique_id",          limit: 255
+    t.integer  "order",              limit: 4
   end
 
   add_index "showcases", ["order"], name: "index_showcases_on_order", using: :btree
   add_index "showcases", ["published"], name: "index_showcases_on_published", using: :btree
   add_index "showcases", ["unique_id"], name: "index_showcases_on_unique_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "display_name"
-    t.string   "email",              default: "", null: false
-    t.integer  "sign_in_count",      default: 0
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",         limit: 255
+    t.string   "last_name",          limit: 255
+    t.string   "display_name",       limit: 255
+    t.string   "email",              limit: 255, default: "", null: false
+    t.integer  "sign_in_count",      limit: 4,   default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "username"
+    t.string   "current_sign_in_ip", limit: 255
+    t.string   "last_sign_in_ip",    limit: 255
+    t.string   "username",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",              limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",  null: false
-    t.integer  "item_id",    null: false
-    t.string   "event",      null: false
-    t.string   "whodunnit"
-    t.text     "object"
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",  limit: 255,   null: false
+    t.integer  "item_id",    limit: 4,     null: false
+    t.string   "event",      limit: 255,   null: false
+    t.string   "whodunnit",  limit: 255
+    t.text     "object",     limit: 65535
     t.datetime "created_at"
   end
 
