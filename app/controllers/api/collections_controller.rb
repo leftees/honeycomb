0fb1f3f7259d5c36ecc931f17d329ec3f1969fcd
Justin Gondron <jgondron@nd.edu>
2015-04-28 10:54:20 -0400

    DEC-242: Removed etag based caching from all api controllers and updated the specs to test for content based etag generation instead.

--- app/controllers/api/collections_controller.rb
+++ app/controllers/api/collections_controller.rb
@@ -2,20 +2,15 @@ module API
   class CollectionsController < APIController
     def index
       @collections = Collection.all
-      if stale?(@collections)
-        respond_to do |format|
-          format.json { render json: GenerateCollectionJSON.call(@collections) }
-        end
+      respond_to do |format|
+        format.json { render json: GenerateCollectionJSON.call(@collections) }
       end
     end
 
     def show
       @collection = Collection.find(params[:id])
-
-      if stale?(@collection)
-        respond_to do |format|
-          format.json { render json: GenerateCollectionJSON.call(@collection) }
-        end
+      respond_to do |format|
+        format.json { render json: GenerateCollectionJSON.call(@collection) }
       end
     end
   end
