43ae8863552b3638e04aee23776aef6d69cd2962
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-04 11:58:35 -0400

    refactor showcase spec.

--- spec/decorators/v1/showcase_json_decorator_spec.rb
+++ spec/decorators/v1/showcase_json_decorator_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe V1::ShowcaseJSONDecorator do
   subject { described_class.new(showcase) }
 
-  let(:collection) { double(Collection, id: 1, unique_id: 'colasdf', title: 'title title') }
-  let(:showcase) { double(Showcase, id: 1, sections: [], description: nil, unique_id: 'adsf', title: 'title title', collection: collection, honeypot_image: honeypot_image) }
-  let(:honeypot_image) { double(HoneypotImage, json_response: 'json_response') }
-  let(:json) { double }
+  let(:showcase) { double(Showcase) }
 
   describe 'generic fields' do
-    [:id, :title, :description, :unique_id, :image, :collection, :updated_at].each do |field|
+    [:id, :description, :unique_id, :image, :collection, :updated_at].each do |field|
       it "responds to #{field}" do
         expect(subject).to respond_to(field)
       end
@@ -17,27 +14,43 @@ RSpec.describe V1::ShowcaseJSONDecorator do
   end
 
   describe '#at_id' do
+    let(:showcase) { double(Showcase, unique_id: "showcae_id") }
+
     it 'returns the path to the id' do
-      expect(subject.at_id).to eq('http://test.host/v1/showcases/adsf')
+      expect(subject.at_id).to eq('http://test.host/v1/showcases/showcae_id')
     end
   end
 
   describe '#collection_url' do
+    let(:collection) { double(Collection, unique_id: "collection_id") }
+    let(:showcase) { double(Showcase, collection: collection) }
+
     it 'returns the path to the items' do
-      expect(subject.collection_url).to eq('http://test.host/v1/collections/colasdf')
+      expect(subject.collection_url).to eq('http://test.host/v1/collections/collection_id')
     end
   end
 
   describe '#description' do
+    let(:showcase) { double(Showcase, description: "description")}
+
+    it "passes description to showcase#description" do
+      expect(showcase).to receive(:description).and_return("description")
+      expect(subject.description).to eq("description")
+    end
+
     it 'converts null to empty string' do
-      expect(subject.description).to eq('')
+      expect(subject.description).to receive(:to_s)
+      subject.description
     end
   end
 
   describe '#slug' do
-    it 'Calls the slug generator' do
-      expect(CreateURLSlug).to receive(:call).with(collection.title).and_return('slug')
-      expect(subject.slug).to eq('slug')
+    let(:showcase) { double(Showcase, title: "title") }
+
+    it "calls the slug generator" do
+      expect(CreateURLSlug).to receive(:call).with(showcase.title).and_return("slug")
+
+      expect(subject.slug).to eq("slug")
     end
   end
 
@@ -82,15 +95,24 @@ RSpec.describe V1::ShowcaseJSONDecorator do
     end
   end
 
+  describe "#image" do
+    let(:showcase) { double(Showcase, honeypot_image: honeypot_image) }
+    let(:honeypot_image) { double(HoneypotImage, json_response: "json_response") }
 
-  describe '#image' do
-    it 'gets the honeypot_image json_response' do
-      expect(honeypot_image).to receive(:json_response).and_return('json_response')
-      expect(subject.image).to eq('json_response')
+    it "gets the honeypot_image json_response" do
+      expect(honeypot_image).to receive(:json_response).and_return("json_response")
+      expect(subject.image).to eq("json_response")
+    end
+
+    it "gets the honeypot_image from the exhibit" do
+      expect(showcase).to receive(:honeypot_image).and_return(honeypot_image)
+      subject.image
     end
   end
 
   describe '#sections' do
+    let(:showcase) { double(Showcase, sections: [] )}
+
     it 'uses the section query object ordered' do
       expect_any_instance_of(SectionQuery).to receive(:ordered).and_return(['results'])
       expect(subject.sections).to eq(['results'])
@@ -103,6 +125,8 @@ RSpec.describe V1::ShowcaseJSONDecorator do
   end
 
   describe '#display' do
+    let(:json) { double }
+
     it 'calls the partial for the display' do
       expect(json).to receive(:partial!).with('/v1/showcases/showcase', showcase_object: showcase)
       subject.display(json)
