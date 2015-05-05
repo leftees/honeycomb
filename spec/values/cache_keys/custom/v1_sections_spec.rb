5ae7ad3269a203377a49708a4e2ec89094724520
Justin Gondron <jgondron@nd.edu>
2015-05-05 16:15:00 -0400

    DEC-244: Fixing remaining hound errors.

--- spec/values/cache_keys/custom/v1_sections_spec.rb
+++ spec/values/cache_keys/custom/v1_sections_spec.rb
@@ -2,16 +2,10 @@ require "rails_helper"
 
 RSpec.describe CacheKeys::Custom::V1Sections do
   context "show" do
-    let(:decorated_section)
-    {
-      instance_double(V1::SectionJSONDecorator, object: "object",
-                                                item: "item",
-                                                item_children: "item_children",
-                                                next: "next",
-                                                previous: "previous",
-                                                collection: "collection",
-                                                showcase: "showcase")
-    }
+    let(:decorated_section) do
+      method_stubs = { object: "object", item: "item", item_children: "item_children", next: "next", previous: "previous", collection: "collection", showcase: "showcase" }
+      instance_double(V1::SectionJSONDecorator, method_stubs)
+    end
 
     it "uses CacheKeys::ActiveRecord" do
       expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate)
@@ -19,13 +13,8 @@ RSpec.describe CacheKeys::Custom::V1Sections do
     end
 
     it "uses the correct data" do
-      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with(record: ["object",
-                                                                                           "item",
-                                                                                           "item_children",
-                                                                                           "next",
-                                                                                           "previous",
-                                                                                           "collection",
-                                                                                           "showcase"])
+      params = ["object", "item", "item_children", "next", "previous", "collection", "showcase"]
+      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with(record: params)
       subject.show(decorated_section: decorated_section)
     end
   end
