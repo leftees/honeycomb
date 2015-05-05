998461575349f95b100ae3c6794e09003a8bb871
Justin Gondron <jgondron@nd.edu>
2015-05-05 15:56:26 -0400

    DEC-244: Fixing curly brace space

--- spec/values/cache_keys/custom/v1_collections_spec.rb
+++ spec/values/cache_keys/custom/v1_collections_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe CacheKeys::Custom::V1Collections do
   context "index" do
-    let(:collections) { instance_double(Collection)}
+    let(:collections) { instance_double(Collection) }
 
     it "uses CacheKeys::ActiveRecord" do
       expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate)
@@ -10,7 +10,7 @@ RSpec.describe CacheKeys::Custom::V1Collections do
     end
 
     it "uses the correct data" do
-      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with({record: collections})
+      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with({ record: collections })
       subject.index(collections: collections)
     end
   end
@@ -24,7 +24,7 @@ RSpec.describe CacheKeys::Custom::V1Collections do
     end
 
     it "uses the correct data" do
-      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with({record: collection})
+      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with({ record: collection })
       subject.show(collection: collection)
     end
   end
