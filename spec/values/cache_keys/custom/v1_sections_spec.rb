b109144d287efd6e3093f9105894efde6dd889df
Justin Gondron <jgondron@nd.edu>
2015-05-05 16:10:29 -0400

    DEC-244: Fixing remaining hound errors.

--- spec/values/cache_keys/custom/v1_sections_spec.rb
+++ spec/values/cache_keys/custom/v1_sections_spec.rb
@@ -2,13 +2,16 @@ require "rails_helper"
 
 RSpec.describe CacheKeys::Custom::V1Sections do
   context "show" do
-    let(:decorated_section) { instance_double(V1::SectionJSONDecorator, object: "object",
-                                                                        item: "item",
-                                                                        item_children: "item_children",
-                                                                        next: "next",
-                                                                        previous: "previous",
-                                                                        collection: "collection",
-                                                                        showcase: "showcase") }
+    let(:decorated_section)
+    {
+      instance_double(V1::SectionJSONDecorator, object: "object",
+                                                item: "item",
+                                                item_children: "item_children",
+                                                next: "next",
+                                                previous: "previous",
+                                                collection: "collection",
+                                                showcase: "showcase")
+    }
 
     it "uses CacheKeys::ActiveRecord" do
       expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate)
@@ -16,13 +19,13 @@ RSpec.describe CacheKeys::Custom::V1Sections do
     end
 
     it "uses the correct data" do
-      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with( record: ["object",
+      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with(record: ["object",
                                                                                            "item",
                                                                                            "item_children",
                                                                                            "next",
                                                                                            "previous",
                                                                                            "collection",
-                                                                                           "showcase"] )
+                                                                                           "showcase"])
       subject.show(decorated_section: decorated_section)
     end
   end
