c891974c1ba16cf05760b500ef3b77d0e855138f
Justin Gondron <jgondron@nd.edu>
2015-05-05 16:39:43 -0400

    DEC-244: Adding attr_reader to follow our convention

--- app/values/cache_keys/generator.rb
+++ app/values/cache_keys/generator.rb
@@ -2,6 +2,8 @@ module CacheKeys
   # Generator handles routing the cache key generation to a given
   # key generator and action for that generator
   class Generator
+    attr_reader :generator, :action, :args
+
     def initialize(key_generator:, action: :generate, **args)
       @generator = key_generator.new
       @action = action
@@ -9,8 +11,8 @@ module CacheKeys
     end
 
     def generate
-      # print @generator.class.name + "." + @action + "='" + @generator.send(@action, **@args) + "'\n"
-      @generator.send(@action, **@args)
+      # print generator.class.name + "." + action + "='" + generator.send(action, **@args) + "'\n"
+      generator.send(@action, **@args)
     end
   end
 end
