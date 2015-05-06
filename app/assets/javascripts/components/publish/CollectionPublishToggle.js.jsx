950fa39028419897ad2fe7a15f44a62591f45f9d
Robert Fox <rfox2@nd.edu>
2015-05-06 14:33:55 -0400

    DEC-276 Create toggle UI element

---
+++ app/assets/javascripts/components/publish/CollectionPublishToggle.js.jsx
@@ -0,0 +1,23 @@
+/** @jsx React.DOM */
+
+var CollectionPublishToggle = React.createClass({
+  propTypes: {
+    collection: React.PropTypes.object.isRequired,
+  }
+  handleClick: function () {
+    this.props.togglePublished();
+  },
+  togglePublished: function () {
+    this.setState(
+      { published: !this.state.published, },
+      this.savePublished
+      );
+  },
+  render: function () {
+    return (
+      <label>
+        <input type="checkbox" checked={this.props.published} onChange={this.handleClick} /> {this.props.publishPanelFieldName}
+      </label>
+    )
+  }
+});
