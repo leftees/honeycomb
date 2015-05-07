fe7df43d28015f3dbc98ec33da7454873b909713
Robert Fox <rfox2@nd.edu>
2015-05-07 16:16:01 -0400

    DEC-298 Create react component for collection published toggle

--- app/assets/javascripts/components/publish/CollectionPublishToggle.js.jsx
+++ app/assets/javascripts/components/publish/CollectionPublishToggle.js.jsx
@@ -3,21 +3,58 @@
 var CollectionPublishToggle = React.createClass({
   propTypes: {
     collection: React.PropTypes.object.isRequired,
+    publishPath: React.PropTypes.string.isRequired,
+    unpublishPath: React.PropTypes.string.isRequired
+  },
+  getInitialState: function() {
+    return {
+      published: this.props.collection.published,
+      published_label: this.publishedLabel(this.props.collection.published)
+    };
   },
   handleClick: function () {
-    this.props.togglePublished();
+    this.togglePublished();
+  },
+  publishedLabel: function (published_status) {
+    var label;
+    if (published_status) {
+      label = 'Published'
+    } else {
+      label = 'Not Published'
+    }
+    return label;
   },
   togglePublished: function () {
-    this.setState(
-      { published: !this.state.published, },
-      this.savePublished
-      );
-  },
-  render: function () {
-    return (
-      <label>
-        <input type="checkbox" checked={this.props.published} onChange={this.handleClick} /> {this.props.publishPanelFieldName}
-      </label>
+    var searchUrl;
+    var published_state;
+    if (this.state.published) {
+      searchUrl = 'http://localhost:3017' + this.props.unpublishPath;
+      published_state = false;
+    } else {
+      searchUrl = 'http://localhost:3017' + this.props.publishPath;
+      published_state = true;
+    }
+    $.ajax({
+      url: searchUrl,
+      dataType: "json",
+      method: "PUT",
+      success: (function(data) {
+        this.setState({
+          published: published_state,
+          published_label: this.publishedLabel(published_state)
+        });
+      }).bind(this),
+      error: (function(xhr, status, err) {
+        console.error(searchUrl, status, err.toString());
+      }).bind(this)
+    });
+},
+render: function () {
+  return (
+    <label>
+    <input type="checkbox" checked={this.state.published} onChange={this.handleClick}/>
+    <span className="toggle"></span><span className="toggle-label">{this.state.published_label}</span>
+    </label>
     )
-  }
+}
 });
