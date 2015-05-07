06b58928c8894c10ebb2587c4c7c9f1f9af31ec4
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-07 11:34:37 -0400

    try to fix this migration.

--- db/migrate/20150505125702_add_created_updated_dates_to_exhibits.rb
+++ db/migrate/20150505125702_add_created_updated_dates_to_exhibits.rb
@@ -3,10 +3,10 @@ class AddCreatedUpdatedDatesToExhibits < ActiveRecord::Migration
     add_column(:exhibits, :created_at, :datetime)
     add_column(:exhibits, :updated_at, :datetime)
 
-    Exhibit.find_each do |e|
-      e.created_at = e.collection.created_at if e.created_at.nil?
-      e.updated_at = e.collection.updated_at if e.updated_at.nil?
-      e.save!
-    end
+    #Exhibit.find_each do |e|
+    #  e.created_at = e.collection.created_at if e.created_at.nil?
+    #  e.updated_at = e.collection.updated_at if e.updated_at.nil?
+    #  e.save!
+    #end
   end
 end
