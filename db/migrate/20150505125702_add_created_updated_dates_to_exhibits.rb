afe495ef300f86fcb738cd4b3b58c27ac2c7e5b5
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-07 09:06:03 -0400

    fix migration to run in pre prod.

--- db/migrate/20150505125702_add_created_updated_dates_to_exhibits.rb
+++ db/migrate/20150505125702_add_created_updated_dates_to_exhibits.rb
@@ -1,10 +1,15 @@
 class AddCreatedUpdatedDatesToExhibits < ActiveRecord::Migration
   def change
-    add_column(:exhibits, :created_at, :datetime)
-    add_column(:exhibits, :updated_at, :datetime)
+    unless column_exists? :exhibits, :created_at
+      add_column(:exhibits, :created_at, :datetime)
+    end
+    unless column_exists? :exhibits, :updated_at
+      add_column(:exhibits, :updated_at, :datetime)
+    end
+
     Exhibit.find_each do |e|
-      e.created_at = e.collection.created_at
-      e.updated_at = e.collection.updated_at
+      e.created_at = e.collection.created_at if e.created_at.nil?
+      e.updated_at = e.collection.updated_at if e.updated_at.nil?
       e.save!
     end
   end
