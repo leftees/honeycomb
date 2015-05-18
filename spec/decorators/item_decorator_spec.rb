ca3d8db655e817615c237e35a7e55fd4f7e89fce
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-18 14:38:08 -0400

    fix item rubocop

--- spec/decorators/item_decorator_spec.rb
+++ spec/decorators/item_decorator_spec.rb
@@ -33,20 +33,19 @@ RSpec.describe ItemDecorator do
     let(:item) { double(Item, id: 1, title: "title", description: "description", transcription: "transcription", manuscript_url: "manuscript_url") }
 
     it "rends the react component" do
-      allow(subject.h).to receive(:form_authenticity_token).and_return('token')
+      allow(subject.h).to receive(:form_authenticity_token).and_return("token")
       expect(subject.h).to receive(:react_component).with(
         "MetaDataForm",
-        {
-          :authenticityToken=>"token",
-          :url=>"/items/1",
-          :method=>"put",
-          :data=> {
-            :item_title=>"title",
-            :item_description=>"description",
-            :item_transcription=>"transcription",
-            :item_manuscript_url=>"manuscript_url"
+          authenticityToken: "token",
+          url: "/items/1",
+          method: "put",
+          data: {
+            item_title: "title",
+            item_description: "description",
+            item_transcription: "transcription",
+            item_manuscript_url: "manuscript_url"
           }
-        })
+        )
 
       subject.edit_form
     end
