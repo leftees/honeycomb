9f0bbd412a070e3852f7caeec519d2eefe25abf3
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-05-21 13:53:52 -0400

    slight wording change.

--- app/assets/javascripts/components/forms/FormServerErrorMsg.js.jsx
+++ app/assets/javascripts/components/forms/FormServerErrorMsg.js.jsx
@@ -5,7 +5,7 @@ var FormServerErrorMsg = React.createClass({
   render: function () {
     return (
       <div className="alert alert-danger" role="alert">
-        The server has reported an error.  The form has not been saved.
+        The server has encountered an error.  The form has not been saved.
       </div>
     );
   }
