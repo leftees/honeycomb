345a1aab96244e3fc79fb85971f004b3e905e85c
Justin Gondron <jgondron@nd.edu>
2015-05-27 14:41:11 -0400

    DEC-359: Fixing hound errors

--- app/values/cache_keys/generator.rb
+++ app/values/cache_keys/generator.rb
@@ -11,7 +11,8 @@ module CacheKeys
     end
 
     def generate
-      key = Rails.application.config.cache_key_header + generator.send(action, **args).to_s
+      Rails.application.config.cache_key_header + generator.send(action, **args).to_s
+      # key = Rails.application.config.cache_key_header + generator.send(action, **args).to_s
       # print generator.class.name + "." + action + "='" + key + "'\n"
       # key
     end
