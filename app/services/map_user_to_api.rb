dbc3d93d4115b575fd36e84ff25caf07796941ed
Jaron Kennel <jaronkk@gmail.com>
2015-06-15 12:04:28 -0400

    Hound

--- app/services/map_user_to_api.rb
+++ app/services/map_user_to_api.rb
@@ -16,7 +16,7 @@ class MapUserToApi
     user.display_name = api_attributes["full_name"]
     user
   rescue StandardError => exception
-    NotifyError.call(exception: exception, args: {username: user.username})
+    NotifyError.call(exception: exception, args: { username: user.username })
     user
   end
 
