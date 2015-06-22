c967d19c4919115856aae1660ede7ff05d94a931
Dan Wolfe <dwolfe2@nd.edu>
2015-06-22 13:54:34 -0400

    remove extra blank line.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-
   config.react.variant = :development
 
   # Raises error for missing translations
