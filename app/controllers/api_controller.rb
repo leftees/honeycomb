95f6c574e5d4897051d9cd3d2471a5f675541caf
Dan Wolfe <dwolfe2@nd.edu>
2015-06-26 10:13:30 -0400

    fix forced error

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,7 +11,6 @@ class APIController < ApplicationController
   # Returns true if the response was rendered
   def rendered_forbidden?(collection)
     can_edit = user_can_edit?(collection)
-    can_edit = false
     unless can_edit
       @errors = { 403 => "User does not have permission to edit this collection." }
       render json: @errors, status: :forbidden
