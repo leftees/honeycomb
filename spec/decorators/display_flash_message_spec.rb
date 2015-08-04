3c53b069481b6e916a34b6b9b587d54ab6b45d57
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-08-04 14:50:47 -0400

    fix tests

--- spec/decorators/display_flash_message_spec.rb
+++ spec/decorators/display_flash_message_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DisplayFlashMessage do
   described_class::KEY_2_CSS_CLASS.each do |key, css_class|
     it "displays the flash messages for notices" do
       flash[key] = "flash"
-      expect(subject.display).to eq("<div class=\"#{css_class}\">\n  flash\n</div>\n")
+      expect(subject.display).to eq("<div data-react-class=\"PageMessage\" data-react-props=\"{&quot;messageText&quot;:&quot;flash&quot;,&quot;css_class&quot;:&quot;#{css_class}&quot;}\"></div>")
     end
   end
 end
