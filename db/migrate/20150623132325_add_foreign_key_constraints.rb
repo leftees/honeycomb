b604b41b208f622f08571dbbf5a5f073a9c9dff1
Justin Gondron <jgondron@nd.edu>
2015-08-04 09:36:23 -0400

    DEC-521: Rewrote using direct execution of queries instead of models so that Jaron can sleep at night.

--- db/migrate/20150623132325_add_foreign_key_constraints.rb
+++ db/migrate/20150623132325_add_foreign_key_constraints.rb
@@ -1,34 +1,44 @@
 class AddForeignKeyConstraints < ActiveRecord::Migration
-  # Asserts that the given relation is empty
-  def empty!(relation:, error_message:)
-    if relation.count > 0
+  # Asserts that the given query results are empty
+  def empty!(query:, error_message:)
+    result = execute query
+    if result.count > 0
       raise "Unable to add foreign keys. #{error_message}"
     end
   end
 
   def change
-    empty!(relation: Section.where("sections.item_id NOT IN (SELECT id FROM items)"),
+    empty!(query: "SELECT * FROM sections WHERE
+                     sections.item_id NOT IN (SELECT id FROM items);",
            error_message: "A section was found that had no associated item.")
 
-    empty!(relation: Section.where("sections.showcase_id NOT IN (SELECT id FROM showcases)"),
+    empty!(query: "SELECT * FROM sections WHERE
+                     sections.showcase_id NOT IN (SELECT id FROM showcases);",
            error_message: "A section was found that had no associated showcase.")
 
-    empty!(relation: Item.where("items.collection_id NOT IN (SELECT id FROM collections)"),
+    empty!(query: "SELECT * FROM items WHERE
+                     items.collection_id NOT IN (SELECT id FROM collections);",
            error_message: "An item was found that had no associated collection.")
 
-    empty!(relation: Item.where("items.parent_id IS NOT NULL AND items.parent_id NOT IN (SELECT id FROM items)"),
+    empty!(query: "SELECT * FROM items WHERE
+                     items.parent_id IS NOT NULL AND
+                     items.parent_id NOT IN (SELECT id FROM items);",
            error_message: "An item was found that had no associated parent item.")
 
-    empty!(relation: Showcase.where("showcases.exhibit_id NOT IN (SELECT id FROM exhibits)"),
+    empty!(query: "SELECT * FROM showcases WHERE
+                     showcases.exhibit_id NOT IN (SELECT id FROM exhibits);",
            error_message: "A showcase was found that had no associated exhibit.")
 
-    empty!(relation: Exhibit.where("exhibits.collection_id NOT IN (SELECT id FROM collections)"),
+    empty!(query: "SELECT * FROM exhibits WHERE
+                     exhibits.collection_id NOT IN (SELECT id FROM collections);",
            error_message: "An exhibit was found that had no associated collection.")
 
-    empty!(relation: CollectionUser.where("collection_users.collection_id NOT IN (SELECT id FROM collections)"),
+    empty!(query: "SELECT * FROM collection_users WHERE
+                     collection_users.collection_id NOT IN (SELECT id FROM collections)",
            error_message: "A collection user was found that had no associated collection.")
 
-    empty!(relation: CollectionUser.where("collection_users.user_id NOT IN (SELECT id FROM users)"),
+    empty!(query: "SELECT * FROM collection_users WHERE
+                     collection_users.user_id NOT IN (SELECT id FROM users)",
            error_message: "A collection user was found that had no associated user.")
 
     add_foreign_key :exhibits, :collections
