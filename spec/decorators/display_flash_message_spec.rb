781e3ca49b3de36639d8ee7811f1665b587136a2
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-08-05 12:49:13 -0400

    hound alignment,

--- spec/decorators/display_flash_message_spec.rb
+++ spec/decorators/display_flash_message_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe DisplayFlashMessage do
   described_class::KEY_2_CSS_CLASS.each do |key, css_class|
     it "displays the flash messages for notices" do
       flash[key] = "flash"
-      result = "<div data-react-class=\"PageMessage\" data-react-props=\"{&quot;messageText&quot;:&quot;flash&quot;,&quot;css_class&quot;:&quot;#{css_class}&quot;}\"></div>"
       expect(subject.h).to receive(:react_component).with(
         "PageMessage",
-         messageText: "flash",
-         css_class: css_class
+        messageText: "flash",
+        css_class: css_class
       ).and_return("message")
       expect(subject.display).to eq("message")
     end
