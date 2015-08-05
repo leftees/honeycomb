5ba57a543948c2ea22cf4ec67f9341f45e4462b6
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-08-05 13:20:04 -0400

    add checkbox to form and update migration to fix data

--- db/migrate/20150804194246_add_show_page_title_to_exhibit.rb
+++ db/migrate/20150804194246_add_show_page_title_to_exhibit.rb
@@ -1,5 +1,7 @@
 class AddShowPageTitleToExhibit < ActiveRecord::Migration
   def change
     add_column :exhibits, :show_page_title, :boolean
+
+    Exhibit.update_all(show_page_title: true)
   end
 end
