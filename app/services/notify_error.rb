19fda8ff32012cb1c3cea0b1cb640dbb387192e5
Jaron Kennel <jaronkk@gmail.com>
2015-08-07 12:30:29 -0400

    Fix an issue with Errbit receiving exceptions with long messages

--- app/services/notify_error.rb
+++ app/services/notify_error.rb
@@ -1,6 +1,10 @@
 module NotifyError
   def self.call(exception:, parameters: {}, component: nil, action: nil)
-    Airbrake.notify(exception,
+    notify_exception = clean_exception(exception)
+    if notify_exception != exception
+      parameters[:original_exception] = exception
+    end
+    Airbrake.notify(notify_exception,
                     component: component,
                     action: action,
                     parameters: parameters,
@@ -8,6 +12,19 @@ module NotifyError
   end
 
   private
+
+  # Errbit indexes the message, but the value can't be indexed if it's larger than 1024 bytes.
+  # This creates a new exception that limits the length of the message.
+  def self.clean_exception(exception)
+    maximum_message_length = 1000 - "#{exception.class}: ".length
+    if exception.message.length > maximum_message_length
+      new_exception = exception.exception(exception.message[0, maximum_message_length])
+      new_exception.set_backtrace(exception.backtrace)
+      new_exception
+    else
+      exception
+    end
+  end
 
   def self.environment_info
     ENV.reject do |k|
--- spec/services/notify_error_spec.rb
+++ spec/services/notify_error_spec.rb
@@ -1,26 +1,46 @@
 require "rails_helper"
 
 RSpec.describe NotifyError do
-  let(:exception) { instance_double(StandardError) }
+  let(:exception) { instance_double(StandardError, message: "An error") }
   let(:parameters) { { test: "test" } }
   let(:component) { "component" }
   let(:action) { "action" }
   let(:expected_environment) { { environment: "environment" } }
-
-  subject { described_class.call(exception: exception, parameters: parameters, component: component, action: action) }
-
-  before do
-    allow(described_class).to receive(:environment_info).and_return(expected_environment)
+  let(:long_message) { (["a"] * 1100).join }
+  let(:long_exception) do
+    begin
+      raise long_message
+    rescue StandardError => e
+      e
+    end
   end
 
-  it "calls Airbrake#notify" do
-    expect(Airbrake).to receive(:notify).with(exception, component: component, action: action, parameters: parameters, cgi_data: expected_environment)
-    subject
-  end
+  describe "call" do
+    subject { described_class.call(exception: exception, parameters: parameters, component: component, action: action) }
 
-  it "calls with default values" do
-    expect(Airbrake).to receive(:notify).with(exception, component: nil, action: nil, parameters: {}, cgi_data: expected_environment)
-    described_class.call(exception: exception)
+    before do
+      allow(described_class).to receive(:environment_info).and_return(expected_environment)
+    end
+
+    it "calls Airbrake#notify" do
+      expect(Airbrake).to receive(:notify).with(exception, component: component, action: action, parameters: parameters, cgi_data: expected_environment)
+      subject
+    end
+
+    it "calls with default values" do
+      expect(Airbrake).to receive(:notify).with(exception, component: nil, action: nil, parameters: {}, cgi_data: expected_environment)
+      described_class.call(exception: exception)
+    end
+
+    context "long message" do
+      let(:exception) { long_exception }
+
+      it "logs the original exception in parameters" do
+        expect(Airbrake).to receive(:notify).
+          with(kind_of(exception.class), hash_including(parameters: { test: "test", original_exception: exception }))
+        subject
+      end
+    end
   end
 
   context "environment_info" do
@@ -37,6 +57,32 @@ RSpec.describe NotifyError do
       filtered_values = ENV.reject { |k| k == "REJECT_KEY" }
       expect(subject).to_not eq(unfiltered_values)
       expect(subject).to eq(filtered_values)
+    end
+  end
+
+  describe "clean_exception" do
+    subject { described_class.clean_exception(exception) }
+
+    it "returns the original exception if the message is a normal length" do
+      expect(subject).to eq(exception)
+    end
+
+    context "long message" do
+      let(:exception) { long_exception }
+
+      it "returns a new exception" do
+        expect(subject).to_not eq(exception)
+      end
+
+      it "limits the message length" do
+        full_message = "#{subject.class}: #{subject.message}"
+        expect(full_message.length).to eq(1000)
+      end
+
+      it "copies the backtrace" do
+        expect(subject.backtrace).to eq(exception.backtrace)
+        expect(subject.backtrace).to be_present
+      end
     end
   end
 end
