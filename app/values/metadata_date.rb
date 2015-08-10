b62f63b2daf1bee2178e8f032ddc01320c26ea6e
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-08-10 12:12:09 -0400

    fix the validations on the date field

--- app/values/metadata_date.rb
+++ app/values/metadata_date.rb
@@ -3,9 +3,9 @@ class MetadataDate
 
   attr_reader :year, :month, :day, :bc, :display_text
 
-  validates :year, numericality: { only_integer: true, allow_nil: false, greater_than_or_equal_to: 0, less_than_or_equal_to: 9999 }
-  validates :month, numericality: { only_integer: true, allow_nil: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
-  validates :day, numericality: { only_integer: true, allow_nil: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
+  validates :year, numericality: { only_integer: true, allow_blank: false, greater_than_or_equal_to: 0, less_than_or_equal_to: 9999 }
+  validates :month, numericality: { only_integer: true, allow_blank: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
+  validates :day, numericality: { only_integer: true, allow_blank: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
   validates :month, presence: true, if: :month_required?
 
   def self.from_hash(hash)
