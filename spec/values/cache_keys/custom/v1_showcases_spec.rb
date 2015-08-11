b1b03e1ea805deeba70d38c2778b956ec99a3f58
Justin Gondron <jgondron@nd.edu>
2015-08-11 14:39:00 -0400

    -Showcases used about and copyright from the associated exhibit, but the cache key generator was not including the exhibit when constructing the key. Fixed. -Fix for https://docs.google.com/document/d/1jL8fFMzSHqAGoo9wTmCAF6a2HgKxr9XbB_LRkx3MB4w/edit#bookmark=id.k1la7qyoc71e

--- spec/values/cache_keys/custom/v1_showcases_spec.rb
+++ spec/values/cache_keys/custom/v1_showcases_spec.rb
@@ -16,23 +16,26 @@ RSpec.describe CacheKeys::Custom::V1Showcases do
   end
 
   context "show" do
-    let(:showcase) do
+    let(:showcase) { instance_double(Showcase, exhibit: "exhibit") }
+    let(:showcase_json) do
       instance_double(V1::ShowcaseJSONDecorator,
                       collection: "collection",
                       sections: "sections",
                       items: "items",
                       next: "next",
-                      object: "showcase object")
+                      object: showcase)
     end
 
     it "uses CacheKeys::ActiveRecord" do
       expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate)
-      subject.show(showcase: showcase)
+      subject.show(showcase: showcase_json)
     end
 
     it "uses the correct data" do
-      expect_any_instance_of(CacheKeys::ActiveRecord).to receive(:generate).with(record: ["showcase object", "collection", "sections", "items", "next"])
-      subject.show(showcase: showcase)
+      expect_any_instance_of(CacheKeys::ActiveRecord).
+        to receive(:generate).
+        with(record: [showcase, showcase_json.collection, showcase.exhibit, showcase_json.sections, showcase_json.items, showcase_json.next])
+      subject.show(showcase: showcase_json)
     end
   end
 end
