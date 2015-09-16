770448df428636fdf383e3823fb5d8c3d5b213bf
Justin Gondron <jgondron@nd.edu>
2015-09-16 11:36:05 -0400

    DEC-498: -Modified to only touch the records that have no unique_id, just in case data exists prior to this migration.

--- db/migrate/20150916134407_copy_items_unique_id_to_user_defined_id.rb
+++ db/migrate/20150916134407_copy_items_unique_id_to_user_defined_id.rb
@@ -1,5 +1,5 @@
 class CopyItemsUniqueIdToUserDefinedId < ActiveRecord::Migration
   def up
-    connection.execute("UPDATE items SET user_defined_id = unique_id;")
+    connection.execute("UPDATE items SET user_defined_id = unique_id WHERE user_defined_id IS NULL OR user_defined_id = '';")
   end
 end
