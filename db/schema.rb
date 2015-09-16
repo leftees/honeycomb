3fb52bd26d829f0c2f88bdd4644507bf9e7be601
Jaron Kennel <jaronkk@gmail.com>
2015-09-16 15:07:52 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818181831) do
+ActiveRecord::Schema.define(version: 20150916134407) do
 
   create_table "collection_users", force: :cascade do |t|
     t.integer  "user_id",       limit: 4, null: false
