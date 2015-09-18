c5237b8bd2ff41dc0b5fbec5a897e27c97eacd41
Jaron Kennel <jaronkk@gmail.com>
2015-09-18 14:57:12 -0400

    Updated example of using honeycomb_solr

--- solrindex.rb
+++ solrindex.rb
@@ -1,5 +1,6 @@
 reload!
-solr = RSolr.connect(url: 'http://localhost:8982/solr/blacklight-core')
+conf = HoneycombSolr.config
+solr = RSolr.connect(url: "http://#{conf.fetch('hostname')}:#{conf.fetch('port')}#{conf.fetch('path')}")
 i = Waggle::Item.from_item(Item.first)
 solr.add(i.as_solr)
 solr.commit
