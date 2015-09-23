4f7fd683ce8d153d0917aac733732e1ff489140d
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2015-09-23 13:48:45 -0400

    updated the item form to have a specialized component for the dropdown.

---
+++ app/assets/javascripts/components/forms/itemMetaDataForm/ItemMetaDataOptionalFields.jsx
@@ -0,0 +1,12 @@
+/*jshint esnext: true */
+
+//app/assets/javascripts/components/forms/ItemMetaDataSelectAdditionalFields.jsx
+var React = require('react');
+var mui = require("material-ui");
+var DropDownMenu = mui.DropDownMenu;
+
+var ItemMetaDataOptionalFields = React.createClass({
+
+});
+
+module.export = ItemMetaDataOptionalFields;
