deb2fbf21053e50c260b48e9e76bc896096383f1
Justin Gondron <jgondron@nd.edu>
2015-10-12 16:58:43 -0400

    no message

--- db/migrate/20151012145430_copy_about_from_exhibits_to_collections.rb
+++ db/migrate/20151012145430_copy_about_from_exhibits_to_collections.rb
@@ -1,10 +1,9 @@
-class MigrateAboutFromExhibitsToCollections < ActiveRecord::Migration
+class CopyAboutFromExhibitsToCollections < ActiveRecord::Migration
   class ExhibitMigration < ActiveRecord::Base
     self.table_name = "exhibits"
   end
 
   def up
-    add_column :collections, :about, :text
     ExhibitMigration.find_each do |exhibit|
       about = exhibit.about
       execute "UPDATE collections SET about = '#{about}' WHERE id = #{exhibit.collection_id}" unless about.nil?
@@ -13,7 +12,6 @@ class MigrateAboutFromExhibitsToCollections < ActiveRecord::Migration
   end
 
   def down
-    remove_column :collections, :about
     ExhibitMigration.reset_column_information
   end
 end
