73f231f34457f7ba8bbbe94c81589f6a7787395a
Justin Gondron <jgondron@nd.edu>
2015-10-15 12:40:35 -0400

    DEC-633: -Removed the down paths from the column add migrations. Letting rails handle the remove column just to be safe.

--- db/migrate/20151013151133_add_images_to_collections.rb
+++ db/migrate/20151013151133_add_images_to_collections.rb
@@ -1,5 +1,5 @@
 class AddImagesToCollections < ActiveRecord::Migration
-  def up
+  def change
     add_column :collections, :image_file_name, :string
     add_column :collections, :image_content_type, :string
     add_column :collections, :image_file_size, :integer
@@ -8,16 +8,5 @@ class AddImagesToCollections < ActiveRecord::Migration
     add_column :collections, :uploaded_image_content_type, :string
     add_column :collections, :uploaded_image_file_size, :integer
     add_column :collections, :uploaded_image_updated_at, :datetime
-  end
-
-  def down
-    remove_column :collections, :image_file_name
-    remove_column :collections, :image_content_type
-    remove_column :collections, :image_file_size
-    remove_column :collections, :image_updated_at
-    remove_column :collections, :uploaded_image_file_name
-    remove_column :collections, :uploaded_image_content_type
-    remove_column :collections, :uploaded_image_file_size
-    remove_column :collections, :uploaded_image_updated_at
   end
 end
--- db/migrate/20151013165130_add_collections_to_honeypot_images.rb
+++ db/migrate/20151013165130_add_collections_to_honeypot_images.rb
@@ -1,9 +1,5 @@
 class AddCollectionsToHoneypotImages < ActiveRecord::Migration
-  def up
+  def change
     add_column :honeypot_images, :collection_id, :integer
-  end
-
-  def down
-    remove_column :honeypot_images, :collection_id
   end
 end
