0b1c97d72c3ed91bacdb694340c928090c333aeb
Justin Gondron <jgondron@nd.edu>
2015-10-15 10:37:39 -0400

    DEC-632 -Fixing hound errors

--- db/migrate/20151015142141_copy_hide_title_from_exhibits_to_collections.rb
+++ db/migrate/20151015142141_copy_hide_title_from_exhibits_to_collections.rb
@@ -6,7 +6,8 @@ class CopyHideTitleFromExhibitsToCollections < ActiveRecord::Migration
   def up
     ExhibitMigration.find_each do |exhibit|
       hide_title_on_home_page = exhibit.hide_title_on_home_page
-      execute "UPDATE collections SET hide_title_on_home_page = #{hide_title_on_home_page} WHERE id = #{exhibit.collection_id}" unless hide_title_on_home_page.nil?
+      execute "UPDATE collections SET hide_title_on_home_page = #{hide_title_on_home_page}
+               WHERE id = #{exhibit.collection_id}" unless hide_title_on_home_page.nil?
     end
     ExhibitMigration.reset_column_information
   end
