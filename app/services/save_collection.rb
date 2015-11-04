0550ea8ccf27d7ab7c3d975ecd9d94a8d9ea1642
Justin Gondron <jgondron@nd.edu>
2015-11-04 15:56:36 -0500

    DEC-627: -Fixing a bug discovered while testing this refactor. If the user did not enter http:// in the url for an external collection, the links were treated as relative. This will now add http:// if it's not given just to correct the common case.

--- app/services/save_collection.rb
+++ app/services/save_collection.rb
@@ -12,6 +12,7 @@ class SaveCollection
 
   def save
     fix_image_param!
+    fix_url!
     collection.attributes = params
     check_unique_id
     if collection.save && process_uploaded_image
@@ -30,6 +31,12 @@ class SaveCollection
   def fix_image_param!
     # sometimes the form is sending an empty image value and this is causing paperclip to delete the image.
     params.delete(:uploaded_image) if params[:uploaded_image].nil?
+  end
+
+  def fix_url!
+    if params[:url].present?
+      params[:url] = "http://#{params[:url]}" unless params[:url][/^https?/]
+    end
   end
 
   def process_uploaded_image
