4346f6ea968d0098be9354d9aaaac3061b7b1e8c
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-01-27 14:33:57 -0500

    Revert "fix additional test/"
    This reverts commit fc5ed7c9b193577e30da4841075ef699b68e3ab2.

--- spec/models/metadata/configuration/config_yml_spec.rb
+++ spec/models/metadata/configuration/config_yml_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-RSpec.describe CreateCollectionConfiguration do
+RSpec.describe Metadata::Configuration do
   Dir.glob(Rails.root.join("config/metadata/*.yml")).each do |path|
     describe path do
       let(:data) { YAML.load_file(path) }
 
-      subject { described_class.new("").send(:base_config) }
+      subject { described_class.new(data) }
 
       it "is valid" do
-        expect(subject[:fields]).to be_kind_of(Array)
-        expect(subject[:fields].first).to be_kind_of(Hash)
+        expect(subject.fields).to be_kind_of(Array)
+        expect(subject.fields.first).to be_kind_of(described_class::Field)
       end
 
       it "doesn't contain duplicate fields" do
