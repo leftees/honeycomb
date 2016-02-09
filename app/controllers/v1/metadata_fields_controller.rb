c32a9a75c62527d9215e5c60e41d9ce0938ad92f
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-02-09 14:38:05 -0500

    update the tests even thought they do not work

--- app/controllers/v1/metadata_fields_controller.rb
+++ app/controllers/v1/metadata_fields_controller.rb
@@ -1,21 +1,11 @@
 module V1
   class MetadataFieldsController < APIController
-    def index
-      collection = CollectionQuery.new.any_find(params[:collection_id])
-      @configuration = CollectionConfigurationQuery.new(collection).find
-      @configuration = V1::MetadataConfigurationJSON.new(@configuration)
-
-      respond_to do |format|
-        format.json { render json: @configuration.to_json }
-      end
-    end
-
     def update
       @collection = CollectionQuery.new.any_find(params[:collection_id])
 
       return if rendered_forbidden?(@collection)
 
-      @return_value = Metadata::UpdateConfigurationField.call(@collection, params[:field])
+      @return_value = Metadata::UpdateConfigurationField.call(@collection, params[:id], params[:fields])
 
       respond_to do |format|
         format.json { render json: { status: @return_value }.to_json }
--- spec/controllers/v1/metadata_fields_controller_spec.rb
+++ spec/controllers/v1/metadata_fields_controller_spec.rb
@@ -3,13 +3,28 @@ require "cache_spec_helper"
 
 RSpec.describe V1::MetadataFieldsController, type: :controller do
   let(:collection) { instance_double(Collection, id: 1) }
-  let(:collections) { [collection] }
+  let(:update_params) { { label: "label" } }
+
+  before(:each) do
+    allow_any_instance_of(CollectionQuery).to receive(:any_find).and_return(collection)
+  end
 
   describe "update" do
     let(:params) { { name: "name" } }
-    subject { put :update, collection_id: 1, field: params }
+    subject { put :update, collection_id: 1, id: "id", fields: update_params }
 
-    it "calls to json on the V1::MetadataConfigurationJSON" do
+    it "queries for the collection" do
+      expect_any_instance_of(CollectionQuery).to receive(:any_find).and_return(collection)
+      subject
+    end
+
+    it "uses the Metadata::UpdateConfigurationField to update the data" do
+      expect(Metadata::UpdateConfigurationField).to receive(:call).with(collection, "id", update_params)
+      subject
+    end
+
+    it "checks the editor permissions" do
+      expect_any_instance_of(described_class).to receive(:user_can_edit?).with(collection)
       subject
     end
   end
