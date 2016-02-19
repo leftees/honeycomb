ed9315f5d976134fa1498ca49ab305943052b24f
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-02-19 11:06:29 -0500

    added validations for the date field.

--- app/validators/metadata_validator.rb
+++ app/validators/metadata_validator.rb
@@ -4,6 +4,7 @@ class MetadataValidator < ActiveModel::Validator
   def validate(record)
     @record = record
     validates_presence
+    validate_dates
   end
 
   private
@@ -17,11 +18,42 @@ class MetadataValidator < ActiveModel::Validator
     end
   end
 
+  def validate_dates
+    configuration.fields.each do |field|
+      value = record.field(field.name)
+      if date_field?(field, value)
+        test_date(field, value)
+      end
+    end
+  end
+
   def configuration
     @configuration ||= CollectionConfigurationQuery.new(record.item.collection).find
   end
 
   def not_present?(field, value)
     field.required && (value.nil? || value.empty?)
+  end
+
+  def date_field?(field, value)
+    field.type == :date && value.present?
+  end
+
+  def test_date(field, value)
+    date = value.first
+    field_name = field.name
+    if !date.valid?
+      date.errors.full_messages.each do |key, msg|
+        record.errors[key] << msg
+      end
+    end
+
+    add_generic_date_errors(field_name, date)
+  end
+
+  def add_generic_date_errors(field, date)
+    if record.errors[field] && !date.to_date
+      record.errors[field] << "Invalid Date"
+    end
   end
 end
--- spec/validators/metadata_validator_spec.rb
+++ spec/validators/metadata_validator_spec.rb
@@ -1,13 +1,15 @@
 
 describe MetadataValidator do
   let(:metadata) { double(errors: errors) }
-  let(:errors) { double({}) }
+  let(:errors) { {} }
   let(:error) { double }
   let(:configuration) { double(Metadata::Configuration, fields: [field]) }
 
   let(:empty_value) { double(empty?: true, nil?: false) }
   let(:nil_value) { double(empty?: false, nil?: true) }
   let(:present_value) { double(empty?: false, nil?: false) }
+
+  let(:date_value) { double(present?: true) }
 
   subject { MetadataValidator.new.validate(metadata) }
 
@@ -16,7 +18,7 @@ describe MetadataValidator do
   end
 
   context "required_field" do
-    let(:field) { double(required: true, name: :field) }
+    let(:field) { double(required: true, name: :field, type: :string) }
 
     it "adds an error if the field is empty?" do
       allow(metadata).to receive(:field).and_return(empty_value)
@@ -43,7 +45,7 @@ describe MetadataValidator do
   end
 
   context "not required_field" do
-    let(:field) { double(required: false, name: :field) }
+    let(:field) { double(required: false, name: :field, type: :string) }
 
     it "adds an error if the field is empty?" do
       allow(metadata).to receive(:field).and_return(empty_value)
@@ -62,6 +64,42 @@ describe MetadataValidator do
     it "does not add an error when the field has value " do
       allow(metadata).to receive(:field).and_return(present_value)
       expect(metadata.errors).to_not receive(:[]).with(field.name)
+
+      subject
+    end
+  end
+
+  context "date field" do
+    let(:field) { double(required: false, name: :field, type: :date) }
+    let(:date_field) { double(MetadataDate, valid?: true, to_date: Time.now) }
+    let(:date_errors) { double(full_messages: { field: "MESSAGE" }) }
+    let(:date_result) { [date_field] }
+
+    before(:each) do
+      allow(metadata).to receive(:field).and_return(date_result)
+    end
+
+    it "uses the validation on the MetadataDate " do
+      expect(date_field).to receive(:valid?).and_return(true)
+      subject
+    end
+
+    it "passes the errors from date_field to " do
+      allow(date_field).to receive(:erros).and_return(date_errors)
+      expect(date_field).to receive(:valid?).and_return(false)
+      expect(date_field).to receive(:errors).and_return(date_errors)
+      expect(metadata.errors).to receive(:[]).with(:field).and_return(error).twice
+      expect(error).to receive("<<").with("MESSAGE")
+
+      subject
+    end
+
+    it "passes a generic errors to the errors" do
+      allow(date_field).to receive(:erros).and_return(date_errors)
+      expect(date_field).to receive(:valid?).and_return(true)
+      expect(date_field).to receive(:to_date).and_return(nil)
+      expect(metadata.errors).to receive(:[]).with(:field).and_return(error).twice
+      expect(error).to receive("<<").with("Invalid Date")
 
       subject
     end
