b820283b613ca413fd43f95179713fb02e018b63
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-02-25 16:03:46 -0500

    hound

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Collection do
   context "foreign key constraints" do
     before(:each) do
       allow_any_instance_of(Metadata::Retrieval).to receive(:valid?).and_return(true)
-      allow_any_instance_of(Metadata::Retrieval).to receive(:set_metadata).and_return(true)      
+      allow_any_instance_of(Metadata::Retrieval).to receive(:set_metadata).and_return(true)
     end
 
     describe "#destroy" do
