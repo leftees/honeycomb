1340529fbda79d7faa7804d1d0d88565c4c23d3a
Justin Gondron <jgondron@nd.edu>
2016-04-27 15:41:50 -0400

    hound

--- app/services/change_list.rb
+++ app/services/change_list.rb
@@ -6,24 +6,30 @@ class ChangeList
     versions.each do |v|
       user = User.find_by_id(v.whodunnit)
       user_name = user.present? ? user.name : nil
-      v1 = v
-      v2 = v.next
-
-      a1 = deserialize(version: v1)
-      a2 = v2.present? ? deserialize(version: v2) : papertrail_object.attributes
-
-      a1.delete("updated_at")
-      a2.delete("updated_at")
-
-      new = a2.to_a - a1.to_a
-      previous = a1.to_a - a2.to_a
-
-      result << { date: v.created_at, user_id: v.whodunnit, user_name: user_name, event: v.event, previous: previous, new: new }
+      diff = changes(version: v,  current_attributes: papertrail_object.attributes)
+      result << { date: v.created_at, user_id: v.whodunnit, user_name: user_name, event: v.event, previous: diff[:previous], new: diff[:new] }
     end
     result
   end
 
   def self.deserialize(version:)
     version.object.present? ? PaperTrail.serializer.load(version.object) : {}
+  end
+
+  # Gets the changes associated with a version. current_attributes must be the attributes
+  # of the object in it's current state (not a version)
+  def self.changes(version:, current_attributes:)
+    next_version = version.next
+
+    attr = deserialize(version: version)
+    next_attr = next_version.present? ? deserialize(version: next_version) : current_attributes
+
+    attr.delete("updated_at")
+    next_attr.delete("updated_at")
+
+    new = next_attr.to_a - attr.to_a
+    previous = attr.to_a - next_attr.to_a
+
+    { previous: previous, new: new }
   end
 end
