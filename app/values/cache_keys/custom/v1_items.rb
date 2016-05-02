dc7d245ade5cadc2658d06ab43d24e7ea453dfea
Robert Fox <rfox2@nd.edu>
2016-05-02 15:04:06 -0400

    DEC-927 Fix for cache expiration on metadata config

--- app/values/cache_keys/custom/v1_items.rb
+++ app/values/cache_keys/custom/v1_items.rb
@@ -3,15 +3,15 @@ module CacheKeys
     # Generator for v1/items_controller
     class V1Items
       def index(collection:)
-        CacheKeys::ActiveRecord.new.generate(record: [collection, collection.items])
+        CacheKeys::ActiveRecord.new.generate(record: [collection, collection.items, collection.collection_configuration])
       end
 
       def show(item:)
-        CacheKeys::ActiveRecord.new.generate(record: [item, item.collection, item.children])
+        CacheKeys::ActiveRecord.new.generate(record: [item, item.collection, item.children, item.collection.collection_configuration])
       end
 
       def showcases(item:)
-        CacheKeys::ActiveRecord.new.generate(record: [item, item.collection, item.showcases])
+        CacheKeys::ActiveRecord.new.generate(record: [item, item.collection, item.showcases, item.collection.collection_configuration])
       end
     end
   end
